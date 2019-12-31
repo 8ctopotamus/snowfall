c101e92c6fc55a56f24c3f9e14e56bd4a09060c9
8ctopotamus <joshnaylor88@gmail.com>
2019-12-31 10:08:33 -0600

    Greatest Amount Chart

--- js/snowfall-charts.js
+++ js/snowfall-charts.js
@@ -1,33 +1,52 @@
-const { all_snowfall_data } = wp_data
+;(function() {
+  const { all_snowfall_data } = wp_data;
 
-function renderDayCharts(numDays) {
-  const canvas = document.getElementById(`most-snow-${numDays}-days`);
-  canvas.height = (all_snowfall_data.length * 10);
-  ctx = canvas.getContext('2d');
+  function getCanvas(id) {
+    const canvas = document.getElementById(id);
+    canvas.height = (all_snowfall_data.length * 10);
+    ctx = canvas.getContext('2d');
+    return ctx;
+  }
 
-  const cityNames = all_snowfall_data.map(city => city['Name']);
-  const dates = all_snowfall_data.map(city => city[`${numDays} days DATE`]);
-  const qty = all_snowfall_data.map(city => city[`${numDays} days QTY`]);
-
-  new Chart(ctx, {
-    type: 'horizontalBar',
-    data: {
-      labels: cityNames,
-      datasets: [{
-        label: 'Inches',
-        data: qty,
-      }]
-    },
-    options: {
-      tooltips: {
-        callbacks: {
-          footer: function(tooltipItem) {
-              return dates[tooltipItem[0].index];
-          }
-        }
+  function renderHorizBarChart(ctx, cityNames, dates, qty) {
+    new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+        labels: cityNames,
+        datasets: [{
+          label: 'Inches',
+          data: qty,
+        }]
       },
-    }
-  });
-}
+      options: {
+        tooltips: {
+          callbacks: {
+            footer: function(tooltipItem) {
+                return dates[tooltipItem[0].index];
+            }
+          }
+        },
+      }
+    });
+  };
 
-[1, 2, 3].forEach(idx => renderDayCharts(idx));
+  function renderDayCharts(idx) {
+    const ctx = getCanvas(`most-snow-${idx}-days`);
+    const cityNames = all_snowfall_data.map(city => city['Name']);
+    const dates = all_snowfall_data.map(city => city[`${idx} days DATE`]);
+    const qty = all_snowfall_data.map(city => city[`${idx} days QTY`]);
+    renderHorizBarChart(ctx, cityNames, dates, qty);
+  };
+
+  function renderGreatestChart() {
+    const ctx = getCanvas('greatest-snowfall');
+    const cityNames = all_snowfall_data.map(city => city['Name']);
+    const dates = all_snowfall_data.map(city => city['GreatestEndingDate']);
+    const qty = all_snowfall_data.map(city => city['Greatest Snowfall']);
+    renderHorizBarChart(ctx, cityNames, dates, qty);
+  };
+
+  [1, 2, 3].forEach(idx => renderDayCharts(idx));
+
+  renderGreatestChart();
+})();
